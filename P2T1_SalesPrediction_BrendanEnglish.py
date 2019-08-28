c68f4b9aaf4ce18d3cf04a45273f4d214855896e
englishb5408 <54634583+englishb5408@users.noreply.github.com>
2019-08-28 11:02:21 -0400

    first submission

---
+++ P2T1_SalesPrediction_BrendanEnglish.py
@@ -0,0 +1,17 @@
+# Take the projected amount of sales and calculate the profit. 
+#8/26/2019
+# CTI-110 P2T1 - Sales Prediction
+# Brendan English
+#
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+                    
+                                
